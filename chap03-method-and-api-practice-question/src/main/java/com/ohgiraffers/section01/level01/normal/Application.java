5c4948dede36d707a3870ff84e50082e61303341
YOUNGHOON-SEO <antifreezu@gmail.com>
2023-10-04 00:51:26 +0900

    commit

---
+++ chap03-method-and-api-practice-question/src/main/java/com/ohgiraffers/section01/level01/normal/Application.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.section01.level01.normal;
+
+import java.io.FilterOutputStream;
+
+import static com.ohgiraffers.section01.level01.normal.RandomMaker.rockPaperScissors;
+import static com.ohgiraffers.section01.level01.normal.RandomMaker.tossCoin;
+
+public class Application {
+
+    /* 실행용 메소드 */
+    public static void main(String[] args) {
+
+        /* RamdomMaker 클래스의 메소드를 호출해서 실행 */
+        System.out.println(rockPaperScissors());
+
+        System.out.println(tossCoin());
+
+    }
+
+}
